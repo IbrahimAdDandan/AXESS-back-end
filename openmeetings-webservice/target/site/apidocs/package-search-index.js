a047a8b2ce89a80e147f48710cf834ddb064689c
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-05-10 19:10:47 +0300

    without target :(

--- openmeetings-webservice/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.openmeetings.webservice"},{"l":"org.apache.openmeetings.webservice.error"},{"l":"org.apache.openmeetings.webservice.util"}]
